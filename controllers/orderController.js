a8eed1e042943f5945df98a382524a9ca1205f65
Prasanna <gramopadhye37@gmail.com>
2023-01-26 16:41:45 +0530

    Small change

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,7 @@
 exports.createOrder = async (req, res, next) => {
     try {
         const {description, id, images, price} = req.body;
+        console.log(description, id, images, price)
         res.status(200).json({
             success: true,
             message: "Order Placed Successfully"
